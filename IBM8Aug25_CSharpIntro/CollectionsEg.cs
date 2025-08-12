a11c0400d78592ff985c94dd3cae41914ea375e0
EZLLeno <EZLLeno@EZL-LEN>
2025-08-12 16:37:36 +0530

    Collection

---
+++ IBM8Aug25_CSharpIntro/CollectionsEg.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBM8Aug25_CSharpIntro
+{
+    internal class CollectionsEg
+    {
+        static void Main()
+        { 
+        
+
+           // List<string> list = new List<string>();
+            List<string> list = new List<string>() {"Ganesh","Mahesh","Dinesh" };
+
+            list.Add("Suresh");
+
+            foreach (var name in list)
+            {
+                Console.WriteLine(  name);
+            }
+
+            Console.WriteLine(list[1]);
+
+            list.Remove("Suresh");
+            list.RemoveAt(1);
+            
+
+            Dictionary<string,string> dict = new Dictionary<string,string>();
+            dict.Add("1001", "Ganesh");
+
+
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine( $"{item.Key}={item.Value}"  );
+            }
+
+
+
+            List<Employee> employees = new List<Employee>() {
+
+             new Employee( ) { EmpID=1, EmpName="Ganesh" },
+              new Employee( ) { EmpID=2, EmpName="Mahesh" },
+              new Employee( ) { EmpID=3, EmpName="Dinesh" },
+              new Employee( ) { EmpID=4, EmpName="Ramesh" },
+
+            };
+
+
+            employees.Add(new Employee( ) { EmpID=1234, EmpName="Ganesh" });
+
+        }
+    }
+}
---
+++ IBM8Aug25_CSharpIntro/GenericEg.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBM8Aug25_CSharpIntro
+{
+    internal class GenericEg
+    {
+
+
+        static void Main()
+        {
+
+            Min<int>(1, 123);
+
+
+            Console.WriteLine(  Min(100,200));
+            Console.WriteLine(Min(500.100, 200.32));
+            Console.WriteLine(Min<string>("xyz", 123.ToString()));
+
+        }
+
+
+        //static int Min( int a , int b) {
+
+        //    if (a < b) { return a; } else return b;
+
+        //}
+
+        //static double  Min(double  a, double  b)
+        //{
+
+        //    if (a < b) { return a; } else return b;
+
+        //}
+
+        //static IComparable  Min(IComparable a, IComparable b)
+        //{
+
+        //    if (a.CompareTo(b) < 0) { return a; } else return b;
+
+        //}
+
+
+        static T Min<T>(T a, T b) where T:IComparable<T>
+        {
+
+            if (a.CompareTo(b) < 0) { return a; } else return b;
+
+        }
+
+        static TR GenericSyntaxEg<T1,T2,TR>(T1 a, T2 b) 
+            where T1 : IComparable<T1>,new()
+            where T2:IDisposable
+            where TR : new()
+        {
+
+            TR obj= new  TR();
+
+
+            return default(TR);
+
+           // return new object();
+           // return obj;
+
+
+        }
+    }
+}
